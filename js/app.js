5c445bb9ec3fc0b94d108b07f316181ca21b9911
jnhustin <jnhustin@gmail.com>
2017-04-25 10:34:35 -0700

    added skills to skill section

--- js/app.js
+++ js/app.js
@@ -5,5 +5,12 @@ angular
 , function($scope) {
     $scope.projectData = projectData;
     $scope.skills = skills;
+
+    for (var section in skills) {
+      var section = skills[section];
+      for (var i = 0; i < section.length - 1; i++) {
+        section[i] += ', ';
+      }
+    }
   }
 ])
