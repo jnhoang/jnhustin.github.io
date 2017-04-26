266d0c3e59cf1f694029fc80bddafb5c7fe0d411
jnhustin <jnhustin@gmail.com>
2017-04-26 10:24:07 -0700

    styling - added padding to containers

--- js/project-data.js
+++ js/project-data.js
@@ -23,7 +23,7 @@ var projectData = {
     name: 'Travel-Sensei'
   , description: 'One day Hackathon app. Connects The Yelp and QPX API\'s to offer highlights to Japan\'s local flavors and destinations.'
   , roles: ['Team lead', 'developer', 'database design', 'backend development']
-  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'Auth(Bcrypt, Passport)', 'Angular', 'MongoDB', 'Mongoose', 'Yelp & QPX(Google flights) API']
+  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'Auth(Bcrypt, Passport)', 'AngularJS', 'MongoDB', 'Mongoose', 'Yelp & QPX(Google flights) API']
   , contributors: ['Mark Woo', 'Nic Vogt']
   , github: 'https://github.com/jnhustin/Travel-Sensei'
   , liveSite: 'https://travelsensei.herokuapp.com/cities/tokyo'
@@ -33,7 +33,7 @@ var projectData = {
     name: 'Texpert'
   , description: 'A concept app that operates as platform to connect users that require tech support to nearby users with the technical knowledge. Individual chat rooms are created to connect two users. A Mongo database maintains user data such as usernames, messages and chat sessions.'
   , roles: ['Developer', 'database design', 'backend development']
-  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'Auth(Bcrypt, Passport)', 'Angular', 'MongoDB', 'Mongoose', 'Sockets.io']
+  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'Auth(Bcrypt, Passport)', 'AngularJS', 'MongoDB', 'Mongoose', 'Sockets.io']
   , contributors: ['Kyle Richardson']
   , github: 'https://github.com/jnhustin/texpert'
   , liveSite: 'http://texpert.herokuapp.com/'
@@ -43,7 +43,7 @@ var projectData = {
     name: 'Animoos'
   , description: 'Animoos is a passion project that incorporated anime into a fun project to learn a grid system library. I chose Materialize as it offers a clean, modern and responsive ui. The site offers the ability to check popular anime titles, or what\'s currently the show in the season.'
   , roles: ['Developer', 'designer']
-  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'Angular', 'Anilist API']
+  , techs: ['CSS', 'HTML', 'JavaScript', 'Node', 'Express', 'AngularJS', 'Anilist API']
   , contributors: []
   , github: 'https://github.com/jnhustin/Animoos'
   , liveSite: 'https://animoos.herokuapp.com/'
@@ -53,8 +53,8 @@ var projectData = {
 
 var skills = {
   Languages: ['CSS3', 'HTML5', 'JavaScript', 'JSON', 'Python', 'Ruby']
-, Frameworks: ['Angular', 'Express', 'Node', 'Rails', 'React', 'jQuery', 'jQuery UI', 'Bootstrap', 'Materialize']
+, Frameworks: ['AngularJS', 'Express', 'Node', 'Rails', 'React', 'jQuery', 'jQuery UI', 'Bootstrap', 'Materialize']
 , Databases: ['ActiveRecord', 'MongoDB', 'Mongoose', 'Parallax.js', 'PostgreSQL', 'Sequelize']
-, Other: ['Agile', 'AJAX', 'Authentication', 'REST Routes', 'Sessions', 'TDD', 'Trello']
+, Other: ['Agile', 'AJAX', 'Authentication', 'RESTful routes', 'Sessions & Passport', 'TDD', 'Trello']
 , 
 };
