8da7de7dbe746157456f5c11a74a4ec0816d0951
jnhustin <jnhustin@gmail.com>
2017-04-26 22:26:34 -0700

    adjust margins

--- js/project-data.js
+++ js/project-data.js
@@ -1,15 +1,15 @@
 var projectData = {
-  // carcassonne: {
-  //   name: 'Carcasonne Castles'
-  // , description: 'A competitive two player game set in the Southern French landscape and inspired by the board game Carcassonne. Opponents take turns drawing tiles to build a landscape of castles, strategically assigning builders to castles under construction. Clever utilization of builders will earn a player more points.'
-  // , roles: ['designer', 'developer', 'game logic']
-  // , techs: ['HTML/CSS', 'JavaScript', 'jQuery']
-  // , contributors: []
-  // , github: 'https://github.com/jnhustin/Carcassonne-Castles'
-  // , liveSite: 'https://jnhustin.github.io/Carcassonne-Castles/'
-  // , videoLink: './vids/carcassonne-demo.mp4'
-  // }
- gameshare: {
+  carcassonne: {
+    name: 'Carcasonne Castles'
+  , description: 'A competitive two player game set in the Southern French landscape and inspired by the board game Carcassonne. Opponents take turns drawing tiles to build a landscape of castles, strategically assigning builders to castles under construction. Clever utilization of builders will earn a player more points.'
+  , roles: ['designer', 'developer', 'game logic']
+  , techs: ['HTML/CSS', 'JavaScript', 'jQuery']
+  , contributors: []
+  , github: 'https://github.com/jnhustin/Carcassonne-Castles'
+  , liveSite: 'https://jnhustin.github.io/Carcassonne-Castles/'
+  , videoLink: './vids/carcassonne-demo.mp4'
+  }
+, gameshare: {
     name: 'Game Share'
   , description: 'GameShare is inspired by a childhood of lending and talking about games with friends in grade school. A user is able to invite friends to a GameShare community. Within a community a user as able to lend/borrow games from other community members. GameShare offers the ability for users to track the games they have borrowed and loaned.'
   , roles: ['designer', 'developer']
@@ -19,16 +19,16 @@ var projectData = {
   , liveSite: 'https://jnhustingameshare.herokuapp.com/'
   , videoLink: './vids/gameshare-demo.mp4'
   }
-// , travelSensei: {
-//     name: 'Travel-Sensei'
-//   , description: 'One day Hackathon app. Connects The Yelp and QPX(Google Flights) API\'s to offer highlights to Japan\'s local flavors and destinations.'
-//   , roles: ['team lead', 'developer', 'database design', 'backend development']
-//   , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Yelp & QPX API']
-//   , contributors: ['Mark Woo, ', 'Nic Vogt']
-//   , github: 'https://github.com/jnhustin/Travel-Sensei'
-//   , liveSite: 'https://travelsensei.herokuapp.com/cities/tokyo'
-//   , videoLink: './vids/travel-sensei-demo.mp4'
-//   }
+, travelSensei: {
+    name: 'Travel-Sensei'
+  , description: 'One day Hackathon app. Connects The Yelp and QPX(Google Flights) API\'s to offer highlights to Japan\'s local flavors and destinations.'
+  , roles: ['team lead', 'developer', 'database design', 'backend development']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Yelp & QPX API']
+  , contributors: ['Mark Woo, ', 'Nic Vogt']
+  , github: 'https://github.com/jnhustin/Travel-Sensei'
+  , liveSite: 'https://travelsensei.herokuapp.com/cities/tokyo'
+  , videoLink: './vids/travel-sensei-demo.mp4'
+  }
 , texpert: {
     name: 'Texpert'
   , description: 'A concept app that operates as platform to connect users that require tech support to nearby users with the technical knowledge. Individual chat rooms are created to connect two users. A Mongo database maintains user data such as usernames, messages and chat sessions.'
@@ -39,16 +39,16 @@ var projectData = {
   , liveSite: 'http://texpert.herokuapp.com/'
   , videoLink: 'vids/texpert-demo.mp4'
   }
-// , animoos: {
-//     name: 'Animoos'
-//   , description: 'Animoos is a passion project that incorporated anime into a fun project to learn a grid system library. I chose Materialize as it offers a clean, modern and responsive ui. The site offers the ability to check popular anime titles, or what\'s currently the show in the season.'
-//   , roles: ['developer', 'designer']
-//   , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'AngularJS', 'Anilist API']
-//   , contributors: []
-//   , github: 'https://github.com/jnhustin/Animoos'
-//   , liveSite: 'https://animoos.herokuapp.com/'
-//   , videoLink: './vids/animoos-demo.mp4'
-//   } 
+, animoos: {
+    name: 'Animoos'
+  , description: 'Animoos is a passion project that incorporated anime into a fun project to learn a grid system library. I chose Materialize as it offers a clean, modern and responsive ui. The site offers the ability to check popular anime titles, or what\'s currently the show in the season.'
+  , roles: ['developer', 'designer']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'AngularJS', 'Anilist API']
+  , contributors: []
+  , github: 'https://github.com/jnhustin/Animoos'
+  , liveSite: 'https://animoos.herokuapp.com/'
+  , videoLink: './vids/animoos-demo.mp4'
+  } 
 };
 
 var skills = {
