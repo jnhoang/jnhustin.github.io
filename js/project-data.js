777b10a9f60f2ed0b539f3e0969b61db51801546
jnhustin <jnhustin@gmail.com>
2017-04-26 21:06:16 -0700

    added spacing between sections

--- js/project-data.js
+++ js/project-data.js
@@ -2,8 +2,8 @@ var projectData = {
   carcassonne: {
     name: 'Carcasonne Castles'
   , description: 'A competitive two player game set in the Southern French landscape and inspired by the board game Carcassonne. Opponents take turns drawing tiles to build a landscape of castles, strategically assigning builders to castles under construction. Clever utilization of builders will earn a player more points.'
-  , roles: ['designer, ', 'developer, ', 'game logic']
-  , techs: ['CSS, ', 'HTML, ', 'JavaScript, ', 'jQuery']
+  , roles: ['designer', 'developer', 'game logic']
+  , techs: ['HTML/CSS', 'JavaScript', 'jQuery']
   , contributors: []
   , github: 'https://github.com/jnhustin/Carcassonne-Castles'
   , liveSite: 'https://jnhustin.github.io/Carcassonne-Castles/'
@@ -12,8 +12,8 @@ var projectData = {
 , gameshare: {
     name: 'Game Share'
   , description: 'GameShare is inspired by a childhood of lending and talking about games with friends in grade school. A user is able to invite friends to a GameShare community. Within a community a user as able to lend/borrow games from other community members. GameShare offers the ability for users to track the games they have borrowed and loaned.'
-  , roles: ['designer, ', 'developer']
-  , techs: ['CSS, ', 'HTML, ', 'JavaScript, ', 'Node, ', 'Express, ', 'Auth(Bcrypt, Passport), ', 'SQL, ', 'PostgreSQL, ', 'Sequelize, ', 'Internet Game Database API']
+  , roles: ['designer', 'developer']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'Passport.js', 'PostgreSQL/Sequelize', 'Internet Game DB API']
   , contributors: []
   , github: 'https://github.com/jnhustin/GameShare'
   , liveSite: 'https://jnhustingameshare.herokuapp.com/'
@@ -22,8 +22,8 @@ var projectData = {
 , travelSensei: {
     name: 'Travel-Sensei'
   , description: 'One day Hackathon app. Connects The Yelp and QPX(Google Flights) API\'s to offer highlights to Japan\'s local flavors and destinations.'
-  , roles: ['Team lead, ', 'developer, ', 'database design, ', 'backend development']
-  , techs: ['CSS, ', 'HTML, ', 'JavaScript, ', 'Node, ', 'Express, ', 'Auth(Bcrypt, Passport), ', 'AngularJS, ', 'MongoDB, ', 'Mongoose, ', 'Yelp & QPX API']
+  , roles: ['team lead', 'developer', 'database design', 'backend development']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Yelp & QPX API']
   , contributors: ['Mark Woo, ', 'Nic Vogt']
   , github: 'https://github.com/jnhustin/Travel-Sensei'
   , liveSite: 'https://travelsensei.herokuapp.com/cities/tokyo'
@@ -32,8 +32,8 @@ var projectData = {
 , texpert: {
     name: 'Texpert'
   , description: 'A concept app that operates as platform to connect users that require tech support to nearby users with the technical knowledge. Individual chat rooms are created to connect two users. A Mongo database maintains user data such as usernames, messages and chat sessions.'
-  , roles: ['Developer, ', 'database design, ', 'backend development']
-  , techs: ['CSS, ', 'HTML, ', 'JavaScript, ', 'Node, ', 'Express, ', 'Auth(Bcrypt, Passport), ', 'AngularJS, ', 'MongoDB, ', 'Mongoose, ', 'Sockets.io']
+  , roles: ['developer', 'database design', 'backend development']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Socket.io']
   , contributors: ['Kyle Richardson']
   , github: 'https://github.com/jnhustin/texpert'
   , liveSite: 'http://texpert.herokuapp.com/'
@@ -42,8 +42,8 @@ var projectData = {
 , animoos: {
     name: 'Animoos'
   , description: 'Animoos is a passion project that incorporated anime into a fun project to learn a grid system library. I chose Materialize as it offers a clean, modern and responsive ui. The site offers the ability to check popular anime titles, or what\'s currently the show in the season.'
-  , roles: ['Developer, ', 'designer']
-  , techs: ['CSS, ', 'HTML, ', 'JavaScript, ', 'Node, ', 'Express, ', 'AngularJS, ', 'Anilist API']
+  , roles: ['developer', 'designer']
+  , techs: ['HTML/CSS', 'JavaScript', 'Node/Express', 'AngularJS', 'Anilist API']
   , contributors: []
   , github: 'https://github.com/jnhustin/Animoos'
   , liveSite: 'https://animoos.herokuapp.com/'
@@ -52,9 +52,9 @@ var projectData = {
 };
 
 var skills = {
-  Languages: ['CSS3', 'HTML5', 'JavaScript', 'JSON', 'Python', 'Ruby']
+  Languages: ['CSS3', 'HTML5', 'JavaScript', 'Python', 'Ruby']
 , Frameworks: ['AngularJS', 'Express', 'Node', 'Rails', 'React', 'jQuery', 'jQuery UI', 'Bootstrap', 'Materialize']
-, Databases: ['ActiveRecord', 'MongoDB', 'Mongoose', 'Parallax.js', 'PostgreSQL', 'Sequelize']
-, Other: ['Agile', 'AJAX', 'Authentication', 'RESTful routes', 'Sessions & Passport', 'TDD', 'Trello']
+, Databases: ['ActiveRecord', 'MongoDB', 'Mongoose', 'PostgreSQL', 'Sequelize']
+, Other: ['Agile', 'AJAX', 'Authentication', 'JSON', 'RESTful routes', 'Sessions & Passport', 'TDD', 'Trello']
 , 
 };
