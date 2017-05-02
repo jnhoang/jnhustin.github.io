9be93ac9b292698c5ee8b3585accbb49e04bd859
jnhustin <jnhustin@gmail.com>
2017-05-02 10:37:26 -0700

    added play/pause on project video clicks

--- js/app.js
+++ js/app.js
@@ -8,7 +8,13 @@ angular
     $scope.skills = skills;
     $scope.visible = false;
 
-
+    
+    $scope.pauseOrPlay = function(e) {
+      var video = angular.element(e.srcElement)[0];
+      video.paused ? video.play() : video.pause();
+    }
+    
+    // Adds comma between list items for horizontal listing
     for (var section in skills) {
       var section = skills[section];
       for (var i = 0; i < section.length - 1; i++) {
