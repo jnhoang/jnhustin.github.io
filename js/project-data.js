7c5dc59d91192851974b8bed1fcffb462020f461
jnhustin <jnhustin@gmail.com>
2017-06-05 09:21:16 -0700

    updated project info

--- js/project-data.js
+++ js/project-data.js
@@ -1,9 +1,9 @@
 var projectData = {
   animoos: {
     name: 'Animoos'
-  , description: 'Animoos is a passion project that incorporated anime into a fun project to learn a grid system library. I chose Materialize as it offers a clean, modern and responsive ui. The site offers the ability to check popular anime titles, or what\'s currently the show in the season.'
+  , description: 'Animoos is a personal website around japanese cartoons/animation, aka anime. The site offers the ability to check popular anime titles, or what\'s currently the show in the season. Users can log in and create lists of their favorites or to be watched anime'
   , roles: ['developer', 'designer']
-  , techs: ['HTML/CSS', 'JavaScript', 'Node.js/Express', 'AngularJS', 'Anilist API']
+  , techs: ['HTML/CSS', 'JavaScript', 'NodeJS/Express', 'AngularJS', 'Anilist API']
   , contributors: []
   , github: 'https://github.com/jnhustin/Animoos'
   , liveSite: 'https://animoos.herokuapp.com/'
@@ -14,7 +14,7 @@ var projectData = {
     name: 'Game Share'
   , description: 'GameShare is inspired by a childhood of lending and talking about games with friends in grade school. A user is able to invite friends to a GameShare community. Within a community a user as able to lend/borrow games from other community members. GameShare offers the ability for users to track the games they have borrowed and loaned.'
   , roles: ['designer', 'developer']
-  , techs: ['HTML/CSS', 'JavaScript', 'Node.js/Express', 'Passport.js', 'PostgreSQL/Sequelize', 'Internet Game DB API']
+  , techs: ['HTML/CSS', 'JavaScript', 'NodeJS/Express', 'Passport.js', 'PostgreSQL/Sequelize', 'Internet Game DB API']
   , contributors: []
   , github: 'https://github.com/jnhustin/GameShare'
   , liveSite: 'https://jnhustingameshare.herokuapp.com/'
@@ -25,7 +25,7 @@ var projectData = {
     name: 'Travel-Sensei'
   , description: 'One day Hackathon app. Connects The Yelp and QPX(Google Flights) API\'s to offer highlights to Japan\'s local flavors and destinations.'
   , roles: ['team lead', 'developer', 'database design', 'backend development']
-  , techs: ['HTML/CSS', 'JavaScript', 'Node.js/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Yelp & QPX API']
+  , techs: ['HTML/CSS', 'JavaScript', 'NodeJS/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Yelp & QPX API']
   , contributors: ['Mark Woo, ', 'Nic Vogt']
   , github: 'https://github.com/jnhustin/Travel-Sensei'
   , liveSite: 'https://travelsensei.herokuapp.com'
@@ -36,7 +36,7 @@ var projectData = {
     name: 'Texpert'
   , description: 'A concept app that operates as platform to connect users that require tech support to nearby users with the technical knowledge. Individual chat rooms are created to connect two users. A Mongo database maintains user data such as usernames, messages and chat sessions.'
   , roles: ['developer', 'database design', 'backend development']
-  , techs: ['HTML/CSS', 'JavaScript', 'Node.js/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Socket.io']
+  , techs: ['HTML/CSS', 'JavaScript', 'NodeJS/Express', 'Passport.js', 'AngularJS', 'MongoDB/Mongoose', 'Socket.io']
   , contributors: ['Kyle Richardson']
   , github: 'https://github.com/jnhustin/texpert'
   , liveSite: 'http://texpert.herokuapp.com/'
@@ -58,7 +58,7 @@ var projectData = {
 
 var skills = {
   Languages: ['HTML5', 'CSS3', 'JavaScript', 'Python', 'Ruby']
-, Frameworks: ['AngularJS', 'Express', 'Node.js', 'Rails', 'React.js', 'jQuery', 'jQuery UI', 'Bootstrap', 'Materialize']
+, Frameworks: ['AngularJS', 'Express', 'NodeJS', 'Rails', 'React.js', 'jQuery', 'jQuery UI', 'Bootstrap', 'Materialize']
 , Databases: ['ActiveRecord', 'MongoDB', 'Mongoose', 'PostgreSQL', 'Sequelize']
 , Other: ['Agile methodology', 'AJAX', 'Promises', 'Authentication', 'JSON', 'RESTful routes', 'Sessions & Passport', 'TDD', 'Trello', 'Jira']
 , 
